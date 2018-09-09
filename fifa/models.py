935d2cb0d5a040054e15ca2c6e88b30c9e69b261
ShivamPatel17 <patel.3063@osu.edu>
2018-09-08 19:11:58 -0700

    added league to models

--- fifa/models.py
+++ fifa/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Player(models.Model):
     first_name = models.CharField(max_length = 100)
     last_name = models.CharField(max_length =100)
+
+class League(models.Model):
+    player = models.ForeignKey('Player',on_delete = models.CASCADE)
